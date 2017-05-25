420caa8b4ec2cb51c401e38deb6bf01d76b3f146
Aldin Mujkic <aldinmujkic44@gmail.com>
2017-05-26 01:24:20 +0200

    loading grunt plugins and register ddefault task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
 // tasks
 module.exports = function(grunt){
+
   // grunt initialization
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    
   });
+
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // register grunt tasks
+  grunt.registerTask('default', [watch]);
 
 }
